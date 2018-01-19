baa33aa153404fac45d2fae8d33eaa65bbeba92b
Vasim Akram Rana <vasim.rana@mobileprogramming.net>
2018-01-19 15:26:45 +0530

    updated code for faq document

--- controllers/documentCtrl.js
+++ controllers/documentCtrl.js
@@ -30,6 +30,7 @@ module.exports.getDocumnetList = function(req, res) {
 * This function is use for to retrieve documnet details but in Open form
 */
 module.exports.openDocDetailsHTML = function(req, res) {
+
   var documentID = req.body.documentID || req.params.documentID || req.query.documentID;
   async.waterfall([
     function(next) {
@@ -121,122 +122,167 @@ function generateDocumentHTML(dictionaryWordsHTML, documentString, req, res, nex
       documentClass  += dictionaryWordsHTML[i].cssClass;
     }
   }
-  var doc = '<html xmlns="http://www.w3.org/1999/xhtml"> \n\
-<head> \n\
-<meta charset="UTF-8"> \n\
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge" /> \n\
-    <meta name="viewport" content="width=device-width, initial-scale=1"> \n\
-    <style> \n\
-        @font-face {\n\
-            font-family: Helvetica;\n\
-            src: url(cms/Helvetica.otf);\n\
-        }\n\
-        body{\n\
-          font-family: Helvetica;\n\
-        }\n\
-        body.fontSizeMedium{\n\
-          font-size: 1.4rem;\n\
-        }\n\
-        .fontSizeLarge{\n\
-          font-size: 1.8rem;\n\
-        }\n\
-        .fontSizeSmall{\n\
-          font-size: 1.0rem;\n\
-        }\n\
-        .tooltip { \n\
-            display: inline; \n\
-            border-bottom: 1px dotted #ccc; \n\
-            zcursor: help; \n\
-            color: #006080; \n\
-            text-align:left; \n\
-        } \n\
-        .tooltip:hover::before{ \n\
-            cursor:pointer; \n\
-            content: ""; \n\
-            position: absolute; \n\
-            left: 5%; \n\
-            right: 5%; \n\
-            margin-top: 77px; \n\
-            font-size: 1.0rem; \n\
-            background-color: #4B4B4D; \n\
-            color: #fff; \n\
-            padding: 15px 15px; \n\
-            z-index:2;\n\
-            padding-top: 0px;\n\
-            line-height: 1.75rem;\n\
-            -webkit-box-shadow: 0px 6px 23px 0px rgba(75,75,77,0.33); \n\
-            box-shadow: 0px 6px 23px 0px rgba(75,75,77,0.33);\n\
-        } \n\
-        .tooltip:hover::after{ \n\
-            cursor:pointer; \n\
-            content: ""; \n\
-            position: absolute; \n\
-            left: 5%; \n\
-            right: 5%; \n\
-            margin-top: 33px; \n\
-            font-size: 1.4rem; \n\
-            background-color: #4B4B4D; \n\
-            color: #509BC3; \n\
-            font-weight: 600;\n\
-            padding: 15px 15px; \n\
-            padding-botom:5px;\n\
-            -webkit-box-shadow: 0px -6px 23px 0px rgba(75,75,77,0.33); \n\
-            box-shadow: 0px -6px 23px 0px rgba(75,75,77,0.33);\n\
-        }\n\
-        .arrow{\n\
-            cursor:pointer; \n\
-            display: none;\n\
-            font-size: 31px;\n\
-            color: #4B4B4D;\n\
-            min-width: 30px;\n\
-            min-height: 12px;\n\
-            position: absolute;\n\
-            background-repeat: no-repeat;\n\
-            margin-top: 10px;\n\
-            margin-left: -50px;\n\
-            z-index: 3;\n\
-        }\n\
-        .tooltip:hover .arrow{cursor:pointer;display:inline-block}\n\
-        .tooltip:hover .tooltiptext {\n\
-            cursor:pointer; \n\
-            visibility: visible;\n\
-            opacity: 1;\n\
-        }\n\
-        body.fontSizeLarge .tooltip:hover::after{\n\
-          font-size:1.8rem;\n\
-        }\n\
-        body.fontSizeSmall .tooltip:hover::after{\n\
-          font-size:1.0rem;\n\
-        }\n\
-        body.fontSizeLarge .tooltip:hover::before{\n\
-          font-size:1.4rem;\n\
-        }\n\
-        body.fontSizeSmall .tooltip:hover::before{\n\
-          font-size:0.8rem;\n\
-        }\n\
-        body.fontSizeLarge .arrow{\n\
-          margin-top: 17px;\n\
-        }\n\
-        .content{   \n\
-          width: 100%;\n\
-        }\n\
-        .content div{\n\
-          margin: 50px 30px;\n\
-        }\n\
-        .content h3{    \n\
-          width: 100%;\n\
-          margin: 7px 0px;\n\
-          text-align: center;\n\
-        }    \n\
-        .content p{    \n\
-          margin: 7px 0;\n\
-        }\n\
-        '+documentClass+'\n\
-    </style>\n\
-</head>\n\
-    <body ontouchstart  onclick="" class="fontSizeMedium" style="text-align:left;">\n\
-    '+ documentString+'\n\
-    </body>\n\
-</html>';
+  var doc = `<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        @font-face {
+            font-family: Helvetica;
+            src: url(cms/Helvetica.otf);
+        }
+        body{
+          font-family: Helvetica;
+        }
+        body.fontSizeMedium{
+          font-size: 1.4rem;
+        }
+        .fontSizeLarge{
+          font-size: 1.8rem;
+        }
+        .fontSizeSmall{
+          font-size: 1.0rem;
+        }
+        .tooltip {
+            display: inline;
+            border-bottom: 1px dotted #ccc;
+            zcursor: help;
+            color: #006080;
+            text-align:left;
+        }
+        .tooltip:hover::before{
+            cursor:pointer;
+            content: "";
+            position: absolute;
+            left: 5%;
+            right: 5%;
+            margin-top: 77px;
+            font-size: 1.0rem;
+            background-color: #4B4B4D;
+            color: #fff;
+            padding: 15px 15px;
+            z-index:2;
+            padding-top: 0px;
+            line-henight: 1.75rem;
+            -webkit-box-shadow: 0px 6px 23px 0px rgba(75,75,77,0.33);
+            box-shadow: 0px 6px 23px 0px rgba(75,75,77,0.33);
+        }
+        .tooltip:hover::after{
+            cursor:pointer;
+            content: "";
+            position: absolute;
+            left: 5%;
+            right: 5%;
+            margin-top: 33px;
+            font-size: 1.4rem;
+            background-color: #4B4B4D;
+            color: #509BC3;
+            font-weight: 600;
+            padding: 15px 15px;
+            padding-botom:5px;
+            -webkit-box-shadow: 0px -6px 23px 0px rgba(75,75,77,0.33);
+            box-shadow: 0px -6px 23px 0px rgba(75,75,77,0.33);
+        }
+        .arrow{
+            cursor:pointer;
+            display: none;
+            font-size: 31px;
+            color: #4B4B4D;
+            min-width: 30px;
+            min-height: 12px;
+            position: absolute;
+            background-repeat: no-repeat;
+            margin-top: 10px;
+            margin-left: -50px;
+            z-index: 3;
+        }
+        .tooltip:hover .arrow{cursor:pointer;display:inline-block}
+        .tooltip:hover .tooltiptext {
+            cursor:pointer;
+            visibility: visible;
+            opacity: 1;
+        }
+        body.fontSizeLarge .tooltip:hover::after{
+          font-size:1.8rem;
+        }
+        body.fontSizeSmall .tooltip:hover::after{
+          font-size:1.0rem;
+        }
+        body.fontSizeLarge .tooltip:hover::before{
+          font-size:1.4rem;
+        }
+        body.fontSizeSmall .tooltip:hover::before{
+          font-size:0.8rem;
+        }
+        body.fontSizeLarge .arrow{
+          margin-top: 17px;
+        }
+        .content{
+          width: 100%;
+        }
+        .content div{
+          margin: 50px 30px;
+        }
+        .content .panel{
+          margin: 0px 0px;
+        }
+        .content h3{
+          width: 100%;
+          margin: 7px 0px;
+          text-align: center;
+        }
+        .content p{
+          margin: 7px 0;
+        }
+        .content .accordion {
+            margin: 1px 0;
+        }
+        .accordion {
+                      background-color: #eee;
+                      color: #444;
+                      cursor: pointer;
+                      padding: 5px;
+                      width: 100%;
+                      border: none;
+                      text-align: left;
+                      outline: none;
+                      font-size: 15px;
+                      transition: 0.4s;
+                  }
+
+                  .panel {
+                      padding: 0 5px;
+                      background-color: white;
+                      max-height: 0;
+                      overflow: hidden;
+                      transition: max-height 0.2s ease-out;
+                  }
+        ` + documentClass+
+        `</style>
+    </head>
+      <body ontouchstart  onclick="" class="fontSizeMedium" style="text-align:left;">`+ documentString+
+    `</body>
+    <script>
+      var acc = document.getElementsByClassName("accordion");
+      var allpanel = document.getElementsByClassName("panel");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+              this.classList.toggle("active");
+              var panel = this.nextElementSibling;
+              if (panel.style.maxHeight) {
+                  panel.style.maxHeight = null;
+              } else {
+                  for (i = 0; i < allpanel.length; i++) {
+                      allpanel[i].removeAttribute("style");
+                  }
+                  panel.style.maxHeight = panel.scrollHeight + "px";
+              }
+          });
+      }
+  </script>
+</html>`;
   next(null, doc);
 }
